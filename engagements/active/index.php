3808eb79c9c15f5abb726a3b8a2051b0b7e9501f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-27 18:51:13 -0500

    update

---
+++ engagements/active/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/styles/styles.css?v=<?php echo time(); ?>">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+
+    <title>Home</title>
+</head>
+<body>
+
+
+    <section class="layout">
+
+        <!-- Sidebar -->
+            <div class="sidebar ps-2">
+                <h1>
+                    Logo Here
+                </h1>
+
+                <div class="pt-4"></div>
+
+                <button type="button" class="btn btn-secondary">
+                    Add New Engagement +
+                </button>
+
+                <div class="pt-4"></div>
+
+                <ul class="list-unstyled ps-4">
+                    <li class="">
+                        <a href="" class="text-decoration-none text-black fw-bold"><i class="bi bi-columns-gap text-primary" style="-webkit-text-stroke: 1px;"></i>
+                        &nbsp;&nbsp;Dashboard</a>
+                    </li>
+
+                    <hr style="color: gray !important; width: 75% !important; text-align: center !important;">
+
+                    <li class="pb-3">
+                        <a href="" class="text-decoration-none text-black fw-bold"><i class="bi bi-folder text-primary" style="-webkit-text-stroke: 1px;"></i>&nbsp;&nbsp;Engagements</a>
+                    </li>
+                    <ul class="list-unstyled">
+                        <li class="ps-4 pb-3">
+                            <a href="" class="text-decoration-none text-black"><i class="bi bi-vector-pen"></i>&nbsp;&nbsp;Draft</a>
+                        </li>
+                        <li class="ps-4 pb-3">
+                            <a href="" class="text-decoration-none text-black"><i class="bi bi-check-circle"></i>&nbsp;&nbsp;Active</a>
+                        </li>
+                        <li class="ps-4 pb-3">
+                            <a href="" class="text-decoration-none text-black"><i class="bi bi-eye"></i>&nbsp;&nbsp;In Review</a>
+                        </li>
+                        <li class="ps-4">
+                            <a href="" class="text-decoration-none text-black"><i class="bi bi-archive"></i>&nbsp;&nbsp;Completed</a>
+                        </li>
+                    </ul>
+
+                    <hr style="color: gray !important; width: 75% !important; text-align: center !important;">
+
+                    <li class="pb-3">
+                        <a href="" class="text-decoration-none text-black fw-bold"><i class="bi bi-diagram-3 text-primary" style="-webkit-text-stroke: 1px;"></i>&nbsp;&nbsp;Organization</a>
+                    </li>
+                    <ul class="list-unstyled">
+                        <li class="ps-4 pb-3">
+                            <a href="" class="text-decoration-none text-black"><i class="bi bi-briefcase"></i>&nbsp;&nbsp;Clients</a>
+                        </li>
+                        <li class="ps-4 pb-3">
+                            <a href="" class="text-decoration-none text-black"><i class="bi bi-person-gear"></i>&nbsp;&nbsp;Users</a>
+                        </li>
+                        <li class="ps-4">
+                            <a href="" class="text-decoration-none text-black"><i class="bi bi-gear"></i>&nbsp;&nbsp;Settings</a>
+                        </li>
+                    </ul>
+
+                </ul>
+
+            </div>
+        <!-- end Sidebar -->
+
+        <!-- Header -->
+            <div class="header d-flex align-items-center justify-content-end" style="height: 60px; background-color: rgb(236,241,247);">
+                <div class="pe-3 d-flex align-items-center gap-2">
+                    <div class="circle-with-text">
+                        GM
+                    </div>
+                    <span>Garrett Morgan</span>
+                    <i class="bi bi-caret-down-fill"></i>
+                </div>
+            </div>
+        <!-- end Header -->
+
+        
+
+      </section>
+
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
+</html>
