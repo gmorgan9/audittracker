0e364bd3f0b45e976ff000b920e0286624791c9b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-27 18:22:19 -0500

    update

--- index.php
+++ index.php
@@ -201,9 +201,9 @@
                             <div class="card-body">
                                 <div class="">
                                     <p class="fw-bold mb-2">LivePerson</p>
-                                </div>
-                                <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                                     <div class="progress-bar" style="width: 25%">25%</div>
+                                    <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                                         <div class="progress-bar" style="width: 25%">25%</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
