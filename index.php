a89594e24ff1b49c6bc7db98621edcc4b54e9835
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-27 18:33:14 -0500

    update

--- index.php
+++ index.php
@@ -188,9 +188,9 @@
 
 
         <div class="body_2">
-            <div class="spacer" style="height: 150px;"></div>
+            <div class="spacer" style="height: 100px;"></div>
 
-            <h5 class="ps-3 mt-4">
+            <h5 class="ps-3">
                 Active Engagements
             </h5>
             <div class="card" style="width: 40rem; height: 31.5rem;">
