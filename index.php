b3f3c8357d7b8fc9fe74485117ddf0b2a1e5b5f4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-27 10:49:43 -0500

    update

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
             <div class="pt-4"></div>
 
             <ul class="list-unstyled ps-4">
-                <li class="pb-3">
+                <li class="">
                     <a href="" class="text-decoration-none text-black fw-bold"><i class="bi bi-columns-gap text-primary" style="-webkit-text-stroke: 1px;"></i>
                     &nbsp;&nbsp;Dashboard</a>
                 </li>
