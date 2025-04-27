9be015deb2b8abe843c48862306a06048dce4373
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-27 11:29:44 -0500

    update

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
 
 
         <div class="header d-flex align-items-center justify-content-end" style="height: 60px; background-color: rgb(236,241,247);">
-            <div class=" d-flex align-items-center gap-2">
+            <div class="pe-3 d-flex align-items-center gap-2">
                 <div class="circle-with-text">
                     GM
                 </div>
