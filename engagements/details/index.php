5714df0c906a89d60ef11c60ddef5478a250f551
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-27 22:06:57 -0500

    update

--- engagements/details/index.php
+++ engagements/details/index.php
@@ -151,7 +151,7 @@
         <!-- end Table -->
         
         <!-- Modal -->
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal fade" id="staticBackdrop" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
               <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                   <div class="modal-header">
