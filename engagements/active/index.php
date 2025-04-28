1f33baaee49e1543d0b81064432efd4707f21aee
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-27 19:04:21 -0500

    update

--- engagements/active/index.php
+++ engagements/active/index.php
@@ -94,40 +94,44 @@
         <!-- Table -->
             <div class="table_body ps-2">
 
-                <h6 class="pb-2">
+                <h5 class="pb-2">
                     Active Engagements
-                </h6>
+                </h5>
 
-                <table class="table table-hover">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">First</th>
-                      <th scope="col">Last</th>
-                      <th scope="col">Handle</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td>Mark</td>
-                      <td>Otto</td>
-                      <td>@mdo</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">2</th>
-                      <td>Jacob</td>
-                      <td>Thornton</td>
-                      <td>@fat</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">3</th>
-                      <td>John</td>
-                      <td>Doe</td>
-                      <td>@social</td>
-                    </tr>
-                  </tbody>
-                </table>
+                <div class="table_content w-75">
+                    <table class="table table-hover">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">First</th>
+                          <th scope="col">Last</th>
+                          <th scope="col">Handle</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>Mark</td>
+                          <td>Otto</td>
+                          <td>@mdo</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">2</th>
+                          <td>Jacob</td>
+                          <td>Thornton</td>
+                          <td>@fat</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">3</th>
+                          <td>John</td>
+                          <td>Doe</td>
+                          <td>@social</td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                </div>
+                
 
             </div>  
         <!-- end Table -->
