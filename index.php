5df6ea6ecbe7df38be72bedff0701c618485b44f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-28 21:59:04 -0500

    update

--- index.php
+++ index.php
@@ -354,9 +354,20 @@ foreach ($files as $file) {
                           <input type="text" class="form-control" id="name" name="name">
                         </div>
 
-                        <div class="mb-3">
-                          <label for="type" class="form-label">Type</label>
-                          <input type="text" class="form-control" id="type" name="type">
+                        <div class="row">
+                          <div class="col-md-6 mb-3">
+                            <label for="type" class="form-label">Type</label>
+                            <input type="text" class="form-control" id="type" name="type">
+                          </div>
+                          <div class="col-md-6 mb-3">
+                            <label for="status" class="form-label">Status</label>
+                            <select class="form-select" id="status" name="status" required>
+                              <option value="Draft">Draft</option>
+                              <option value="Active">Active</option>
+                              <option value="In Review">In Review</option>
+                              <option value="Completed">Compelted</option>
+                            </select>
+                          </div>
                         </div>
 
                         <div class="row">
@@ -405,15 +416,7 @@ foreach ($files as $file) {
                         </div>
 
                         <div class="row">
-                          <div class="col-md-6 mb-3">
-                            <label for="status" class="form-label">Status</label>
-                            <select class="form-select" id="status" name="status" required>
-                              <option value="Draft">Draft</option>
-                              <option value="Active">Active</option>
-                              <option value="In Review">In Review</option>
-                              <option value="Completed">Compelted</option>
-                            </select>
-                          </div>
+                          
                         </div>
 
                         <div class="row">
