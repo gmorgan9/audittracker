dcb732a9c9925a7b8e4f175a507c01cc2cddf9c7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-29 09:28:16 -0500

    update

--- engagements/in-review/index.php
+++ engagements/in-review/index.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('America/Denver');
 require_once "../../app/database/connection.php"; // Ensure this is correct
-// require_once "../path.php";
+require_once "../../path.php";
 session_start();
 
 ini_set('display_errors', 1);
