b1fcb4486cfa619eb3398c63fa2ba07ca2f5bcdf
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-29 08:00:02 -0500

    update

--- engagements/details/index.php
+++ engagements/details/index.php
@@ -248,7 +248,7 @@ foreach ($files as $file) {
                         $page = isset($_GET['page']) ? $_GET['page'] : 1;
                         $offset = ($page - 1) * $limit;
 
-                        $comment_sql = "SELECT * FROM comments WHERE parent_comment_id=NULL AND engagement_idno='$id' ORDER BY created DESC LIMIT $limit OFFSET $offset";
+                        $comment_sql = "SELECT * FROM comments WHERE parent_comment_id IS NULL AND engagement_idno='$id' ORDER BY created DESC LIMIT $limit OFFSET $offset";
                         $comment_result = mysqli_query($conn, $comment_sql);
                         if($comment_result) {
                             $comment_num_rows = mysqli_num_rows($comment_result);
