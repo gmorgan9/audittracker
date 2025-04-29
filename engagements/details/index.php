ab11a7ae2f4f504ad679d04e25abac5adc28c4b3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-29 09:57:44 -0500

    update

--- engagements/details/index.php
+++ engagements/details/index.php
@@ -296,18 +296,10 @@ foreach ($files as $file) {
                                               </div>
                                             </div>
                                                     
-                                            <div class="row">
-                                              <div class="col-md-6 mb-3">
-                                                <label for="reporting_start" class="form-label">comm_details_parent_comment_id</label>
-                                                <input type="date" class="form-control" id="comm_details_parent_comment_id" name="comm_details_parent_comment_id" value="<?php echo $comm_details_parent_comment_id; ?>">
-                                              </div>
-                                              
-                                            </div>
+                                            
                                             <div class="mb-3">
                                                 <label for="comm_details_comment " class="form-label">comm_details_parent_comment_id</label>
-                                                <textarea class="form-control" id="comm_details_comment " name="comm_details_comment">
-                                                    <?php echo $comm_details_comment ; ?>
-                                                </textarea>
+                                                <textarea class="form-control" id="comm_details_comment " name="comm_details_comment"><?php echo $comm_details_comment ; ?></textarea>
                                               </div>
                                                     
                                             <div class="row">
