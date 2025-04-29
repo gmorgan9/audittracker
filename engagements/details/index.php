53bfee8ffd0c759ff9ed56272a5c37f979aed02e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-29 10:15:39 -0500

    update

--- engagements/details/index.php
+++ engagements/details/index.php
@@ -336,7 +336,7 @@ foreach ($files as $file) {
                                     }}}
                                 ?>
 
-                                <div class="modal fade" id="comment_details<?php echo$comment_id; ?>" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                <div class="modal fade" id="comment_details<?php echo $comm_details_id; ?>" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                                   <div class="modal-dialog modal-dialog-centered modal-lg">
                                     <div class="modal-content">
                                         <div class="modal-header">
