99940de69a231abe0ea8c03a3e1e93ec49adaf81
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-04-29 09:33:18 -0500

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -14,7 +14,7 @@
         </li>
         <hr style="color: gray !important; width: 75% !important; text-align: center !important;">
         <li class="pb-3">
-            <a href="" class="text-decoration-none text-black fw-bold"><i class="bi bi-folder" style="-webkit-text-stroke: 1px;color: rgb(55, 67, 118);"></i>&nbsp;&nbsp;Engagements</a>
+            <a href="<?php BASE_URL; ?>/engagements/" class="text-decoration-none text-black fw-bold"><i class="bi bi-folder" style="-webkit-text-stroke: 1px;color: rgb(55, 67, 118);"></i>&nbsp;&nbsp;Engagements</a>
         </li>
         <ul class="list-unstyled">
             <li class="ps-4 pb-3">
