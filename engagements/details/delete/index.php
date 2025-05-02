f0a61d19d4387e1953d4e9580ef0196f919e5cf6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-05-02 18:21:06 -0500

    update

--- engagements/details/delete/index.php
+++ engagements/details/delete/index.php
@@ -4,12 +4,13 @@ include_once ROOT_PATH . '/app/database/connection.php';
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
+    $idno = $_GET['eidno'];
 
     $stmt = mysqli_prepare($conn, "DELETE FROM comments WHERE idno = ?");
     mysqli_stmt_bind_param($stmt, 'i', $id);
 
     if (mysqli_stmt_execute($stmt)) {
-        header("Location: " . BASE_URL . "/engagements/details/?deleted=true");
+        header("Location: " . BASE_URL . "/engagements/details/?id=" . $idno . "&deleted=true");
         exit;
     } else {
         echo "Error deleting record: " . mysqli_error($conn);
