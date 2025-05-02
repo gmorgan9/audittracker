cf15655c25c84ac884c5486313e583cce6f3364a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-05-02 18:35:43 -0500

    update

--- engagements/details/delete/index.php
+++ engagements/details/delete/index.php
@@ -3,26 +3,26 @@ include_once '../../../path.php';
 include_once ROOT_PATH . '/app/database/connection.php';
 
 if (isset($_GET['id'])) {
-    $id = $_GET['id']; // The parent comment ID
-    $idno = $_GET['eidno']; // The engagement_idno for redirect
+    $id = $_GET['id'];
+    $idno = $_GET['eidno'];
 
     // Step 1: Delete child comments
     $stmt1 = mysqli_prepare($conn, "DELETE FROM comments WHERE parent_comment_id = ?");
-    mysqli_stmt_bind_param($stmt1, 'i', $id);
-    mysqli_stmt_execute($stmt1);
+    mysqli_stmt_bind_param($stmt1, 's', $id); // Changed to 's'
+    if (!mysqli_stmt_execute($stmt1)) {
+        echo "Error deleting child comments: " . mysqli_error($conn);
+    }
     mysqli_stmt_close($stmt1);
 
     // Step 2: Delete the parent comment
     $stmt2 = mysqli_prepare($conn, "DELETE FROM comments WHERE id = ?");
-    mysqli_stmt_bind_param($stmt2, 'i', $id);
-
+    mysqli_stmt_bind_param($stmt2, 's', $id); // Changed to 's'
     if (mysqli_stmt_execute($stmt2)) {
         header("Location: " . BASE_URL . "/engagements/details/?id=" . $idno . "&deleted=true");
         exit;
     } else {
-        echo "Error deleting record: " . mysqli_error($conn);
+        echo "Error deleting parent comment: " . mysqli_error($conn);
     }
-
     mysqli_stmt_close($stmt2);
 } else {
     echo "ID not provided.";
