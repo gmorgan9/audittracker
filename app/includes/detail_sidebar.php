9ee2a040ba0c51dd7db70f1a9819e451caa2ce1f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2025-05-02 17:43:41 -0500

    update

---
+++ app/includes/detail_sidebar.php
@@ -0,0 +1,49 @@
+<div class="table_sidebar ps-2">
+    <h1>
+        Logo Here
+    </h1>
+    <div class="pt-4"></div>
+    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#add_comment" style="background-color: rgb(55, 67, 118); color: white;">
+        Add QA Comment
+    </button>
+    <div class="pt-4"></div>
+    <ul class="list-unstyled ps-4">
+        <li class="">
+            <a href="<?php BASE_URL; ?>/" class="text-decoration-none text-black fw-bold"><i class="bi bi-columns-gap" style="-webkit-text-stroke: 1px;color: rgb(55, 67, 118);"></i>
+            &nbsp;&nbsp;Dashboard</a>
+        </li>
+        <hr style="color: gray !important; width: 75% !important; text-align: center !important;">
+        <li class="pb-3">
+            <a href="<?php BASE_URL; ?>/engagements/" class="text-decoration-none text-black fw-bold"><i class="bi bi-folder" style="-webkit-text-stroke: 1px;color: rgb(55, 67, 118);"></i>&nbsp;&nbsp;Engagements</a>
+        </li>
+        <ul class="list-unstyled">
+            <li class="ps-4 pb-3">
+                <a href="<?php BASE_URL; ?>/engagements/draft/" class="text-decoration-none text-black"><i class="bi bi-vector-pen"></i>&nbsp;&nbsp;Draft</a>
+            </li>
+            <li class="ps-4 pb-3">
+                <a href="<?php BASE_URL; ?>/engagements/active/" class="text-decoration-none text-black"><i class="bi bi-check-circle"></i>&nbsp;&nbsp;Active</a>
+            </li>
+            <li class="ps-4 pb-3">
+                <a href="<?php BASE_URL; ?>/engagements/in-review/" class="text-decoration-none text-black"><i class="bi bi-eye"></i>&nbsp;&nbsp;In Review</a>
+            </li>
+            <li class="ps-4">
+                <a href="<?php BASE_URL; ?>/engagements/completed/" class="text-decoration-none text-black"><i class="bi bi-archive"></i>&nbsp;&nbsp;Completed</a>
+            </li>
+        </ul>
+        <hr style="color: gray !important; width: 75% !important; text-align: center !important;">
+        <li class="pb-3">
+            <a href="" class="text-decoration-none text-black fw-bold"><i class="bi bi-diagram-3" style="-webkit-text-stroke: 1px;color: rgb(55, 67, 118);"></i>&nbsp;&nbsp;Organization</a>
+        </li>
+        <ul class="list-unstyled">
+            <!-- <li class="ps-4 pb-3">
+                <a href="" class="text-decoration-none text-black"><i class="bi bi-briefcase"></i>&nbsp;&nbsp;Clients</a>
+            </li> -->
+            <li class="ps-4 pb-3">
+                <a href="" class="text-decoration-none text-black"><i class="bi bi-person-gear"></i>&nbsp;&nbsp;Users</a>
+            </li>
+            <li class="ps-4">
+                <a href="" class="text-decoration-none text-black"><i class="bi bi-gear"></i>&nbsp;&nbsp;Settings</a>
+            </li>
+        </ul>
+    </ul>
+</div>
